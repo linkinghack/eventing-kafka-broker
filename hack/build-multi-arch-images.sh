f74ca38961f3cf71f9d283bdbfc8863f1b443678
linkinghack <linkinghack@outlook.com>
2022-02-22 21:50:17 +0800

    fix image name

--- hack/build-multi-arch-images.sh
+++ hack/build-multi-arch-images.sh
@@ -11,7 +11,7 @@ docker buildx build --platform linux/amd64,linux/arm64 \
 
 docker buildx build --platform linux/amd64,linux/arm64 \
   -f ./data-plane/docker/Dockerfile \
-  --build-arg JAVA_IMAGE=eclipse-temurin:17.0.2_8-jdk-centos7k \
+  --build-arg JAVA_IMAGE=eclipse-temurin:17.0.2_8-jdk-centos7 \
   --build-arg BASE_IMAGE=eclipse-temurin:17.0.2_8-jre-centos7 \
   --build-arg APP_JAR=dispatcher-1.0-SNAPSHOT.jar \
   --build-arg APP_DIR=dispatcher \
