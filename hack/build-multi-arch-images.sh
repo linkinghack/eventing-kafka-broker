8be48b6f06e91274bbb1559e3f3e6d03a6b29783
linkinghack <linkinghack@outlook.com>
2022-02-22 21:45:25 +0800

    use standard JRE-17

--- hack/build-multi-arch-images.sh
+++ hack/build-multi-arch-images.sh
@@ -2,8 +2,8 @@ docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASSWORD;
 
 docker buildx build --platform linux/amd64,linux/arm64 \
   -f ./data-plane/docker/Dockerfile \
-  --build-arg JAVA_IMAGE=openjdk:17.0.2-jdk \
-  --build-arg BASE_IMAGE=gcr.io/distroless/java-debian11:base-nonroot \
+  --build-arg JAVA_IMAGE=eclipse-temurin:17.0.2_8-jdk-centos7 \
+  --build-arg BASE_IMAGE=eclipse-temurin:17.0.2_8-jre-centos7 \
   --build-arg APP_JAR=receiver-1.0-SNAPSHOT.jar \
   --build-arg APP_DIR=receiver \
   -t "linkinghack/knative-eventing-kafka-broker-receiver:1.2" \
@@ -11,8 +11,8 @@ docker buildx build --platform linux/amd64,linux/arm64 \
 
 docker buildx build --platform linux/amd64,linux/arm64 \
   -f ./data-plane/docker/Dockerfile \
-  --build-arg JAVA_IMAGE=openjdk:17.0.2-jdk \
-  --build-arg BASE_IMAGE=gcr.io/distroless/java-debian11:base-nonroot \
+  --build-arg JAVA_IMAGE=eclipse-temurin:17.0.2_8-jdk-centos7k \
+  --build-arg BASE_IMAGE=eclipse-temurin:17.0.2_8-jre-centos7 \
   --build-arg APP_JAR=dispatcher-1.0-SNAPSHOT.jar \
   --build-arg APP_DIR=dispatcher \
   -t "linkinghack/knative-eventing-kafka-broker-dispatcher:1.2" \
